0d9a4ccb26f8800378c8ed1aed386824a9737518
laoqilian <96591077@qq.com>
2018-07-03 22:26:29 +0800

    aa

--- app/src/main/java/com/loslink/jni/slingshot/MainActivity.java
+++ app/src/main/java/com/loslink/jni/slingshot/MainActivity.java
@@ -2,12 +2,16 @@ package com.loslink.jni.slingshot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 移除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
