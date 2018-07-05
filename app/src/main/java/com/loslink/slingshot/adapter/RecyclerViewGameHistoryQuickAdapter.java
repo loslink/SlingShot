e3f16ef58baab5589c8d865b6776abef988de158
laoqilian <96591077@qq.com>
2018-07-05 22:47:34 +0800

    new

--- app/src/main/java/com/loslink/slingshot/adapter/RecyclerViewGameHistoryQuickAdapter.java
+++ app/src/main/java/com/loslink/slingshot/adapter/RecyclerViewGameHistoryQuickAdapter.java
@@ -6,6 +6,8 @@ import com.loslink.slingshot.R;
 import com.loslink.slingshot.base.BaseViewHolder;
 import com.loslink.slingshot.bean.HistoryBean;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 
@@ -22,7 +24,10 @@ public class RecyclerViewGameHistoryQuickAdapter extends BaseQuickAdapter<Histor
 
     @Override
     protected void convert(final BaseViewHolder helper, HistoryBean item) {
-//        helper.setText(R.id.tv_road, travelMainAddrs);
+        helper.setText(R.id.tv_score, "Score: "+item.getScore());
+        DateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        String sStr = sdf.format(item.getTime());
+        helper.setText(R.id.tv_time, sStr);
     }
 
     
